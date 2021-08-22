259f3fb68e9d3de4e95bdde603170f6b0386db06
oscarnipps <oscarnips@gmail.com>
2021-08-22 13:47:16 +0100

    sign up ui implementation

---
+++ app/src/main/java/com/example/volu/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.example.volu;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
--- app/src/main/java/com/example/volu/login/LoginFragment.java
+++ app/src/main/java/com/example/volu/login/LoginFragment.java
@@ -7,11 +7,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.volu.R;
+import com.example.volu.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding mBinding;
+    private NavController mNavController;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,8 +28,27 @@ public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login,container,false);
-        return view;
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+
+        mBinding.signUp.setOnClickListener(v -> mNavController.navigate(R.id.navigate_to_sign_up));
+
+        mBinding.forgotPassword.setOnClickListener(v -> retrieveUserPassword());
+
+        mBinding.login.setOnClickListener(v -> loginUser());
+
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mNavController = Navigation.findNavController(view);
+    }
+
+    private void retrieveUserPassword() {
+    }
+
+    private void loginUser() {
     }
 
 }
